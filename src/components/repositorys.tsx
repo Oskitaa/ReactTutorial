3c94f51b3e33f6cac6fbefb52d2f5b36c78612a4
Oskitaa <ese.betico.98@gmail.com>
2021-03-05 14:13:29 +0100

    first route

---
+++ src/components/repositorys.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Repository extends Component{
+
+    render(){
+        return(
+            <h1>Hola, estas en repositiocio</h1>
+        )
+    };
+    
+}
+
+export default Repository;
