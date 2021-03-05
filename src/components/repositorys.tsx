f7f2be855fa9c1a44ac90f0c3adadc8fdbba03f6
Oskitaa <ese.betico.98@gmail.com>
2021-03-05 16:54:25 +0100

    añadido nombre, descripcion y enlace a repositorios

--- src/components/repositorys.tsx
+++ src/components/repositorys.tsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React , { useEffect,useState } from "react";
+import { Link } from "react-router-dom";
 
 function Repository(){
-        return(
-            <div>
-                <h1>Hola, estas en repositorio.</h1>
-            </div>
-        )
+
+    interface Repositorio{
+        name : String,
+        description : String,
+        created_at : Date,
+        html_url : String
+    }
+
+    useEffect(() => {
+        fecthRepo();
+    }, []);
+
+    const [repos , setRepos] = useState<Repositorio[]>([]);
+
+    const fecthRepo = async ()=> {
+        const data = await fetch("https://api.github.com/users/oskitaa/repos");
+        const repos = await data.json();
+        setRepos(repos);
+    };
+    
+    return(
+        <div>
+            <h1>Repositorios</h1>
+            {
+                repos.map( repo => (
+                    <div>
+                        <p><a href={`${repo.html_url}`} target="blank" >{repo.name} </a>- <span>{new Date(repo.created_at).getFullYear()}</span></p>
+                        <p>{repo.description}</p>
+                    </div>
+                    
+                ))
+            }
+        </div>
+    )
 }
 
 export default Repository;
